952e1a705ad586f88e0c08ac91c4589b09888b36
Patteco03 <Thiago@192.168.100.5>
2016-11-18 14:16:42 -0200

    CidadeBean - Salvar

--- Coleta/src/main/java/br/com/coleta/bean/CidadeBean.java
+++ Coleta/src/main/java/br/com/coleta/bean/CidadeBean.java
@@ -14,7 +14,6 @@ import br.com.coleta.dao.EstadoDAO;
 import br.com.coleta.domain.Cidade;
 import br.com.coleta.domain.Estado;
 
-
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
@@ -66,6 +65,27 @@ public class CidadeBean implements Serializable {
 			estados = estadoDAO.listar();
 		} catch (RuntimeException erro) {
 			Messages.addFlashGlobalError("Ocorreu um erro ao gerar uma nova cidade");
+			erro.printStackTrace();
+		}
+	}
+
+	public void salvar() {
+
+		try {
+			
+			CidadeDAO cidadeDAO = new CidadeDAO();
+			cidadeDAO.merge(cidade);
+			
+			cidade = new Cidade();
+			
+			EstadoDAO estadoDAO = new EstadoDAO();
+			estados = estadoDAO.listar();
+			cidades = cidadeDAO.listar();
+			
+			
+			Messages.addGlobalInfo("Cidade salva com sucesso!");
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar salvar");
 			erro.printStackTrace();
 		}
 	}
